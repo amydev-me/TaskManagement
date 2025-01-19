589b9639a7f27f6e56f614246f733d37a69e7631
Amy <ampyaephyonaing@gmail.com>
2025-01-19 13:04:22 +0700

    feat: implement task update functionality
    - Add endpoint for updating tasks
    
    - Include express-validator for validation
    
    - Ensure proper authorization using middleware

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-import { Task, TaskStatus } from "../models/task";
-import mongoose from "mongoose";
 import { NotFoundError } from "../errors/not-found-error";
 import { NotAuthorizedError } from "../errors/no-authorize-error";
-
+import { Task, TaskStatus } from "../models/task";
+import mongoose from "mongoose";
 
 const getTasks = async(req: Request, res: Response) => {
     const tasks = await Task.find({
@@ -32,6 +31,32 @@ const newTask = async(req: Request, res: Response) => {
     });
 }
 
+const updateTask = async(req: Request, res: Response) => {
+    const { title, description, due_date, status } = req.body;
+    const taskId = req.params.taskId;
+
+    const task = await Task.findById(taskId);
+    if(!task) {
+        throw new NotFoundError();
+    }
+
+    if(task.ownerId.toString() !== req.currentUser!.id){
+        throw new NotAuthorizedError();
+    }
+
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.dueDate = due_date || task.dueDate;
+    task.status = status || task.status;
+
+    await task.save();
+
+    res.status(201).send({
+        message: "Task updated successfully.",
+        task
+    });
+}
+
 const deleteTask = async(req: Request, res: Response) => {
     const { taskId } = req.params;
     
@@ -50,4 +75,4 @@ const deleteTask = async(req: Request, res: Response) => {
     res.status(204).send({ message: 'Task deleted successfully.' });
 }
 
-export { getTasks, newTask, deleteTask};
+export { getTasks, updateTask, newTask, deleteTask};
--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/require-auth';
-import { deleteTask, getTasks, newTask } from '../controllers/taskController';
+import { deleteTask, getTasks, newTask, updateTask } from '../controllers/taskController';
  
 const router = Router();
 
 router.get('/api/tasks', requireAuth, getTasks);
 router.post('/api/tasks', requireAuth, newTask);
+router.put('/api/tasks/:taskId', requireAuth, updateTask);
 router.delete('/api/tasks', requireAuth, deleteTask);
 
 export { router as taskRouter };
